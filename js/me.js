c28d770b83f79a29b250f70f34174818fa7ddf18
SeanSCao <ssc543@nyu.edu>
2019-02-17 18:27:24 -0500

    Updated Contact Section

--- js/me.js
+++ js/me.js
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 100
+    offset: 500
   });
 
   // Collapse Navbar
